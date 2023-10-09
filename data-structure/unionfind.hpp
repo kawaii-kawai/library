3148deec5cb40b3fb65d9d23433f21078eb462f5
kawaii-kawai <kawaiatsuki2008@gmail.com>
2023-10-09 16:38:30 +0900

    update

--- data-structure/unionfind.hpp
+++ data-structure/unionfind.hpp
@@ -3,25 +3,28 @@
 #include <algorithm>
 #include <vector>
 
-struct UnionFind {
+class UnionFind {
+  private:
     int size_;
-    std::vector<int> par;
+    std::vector<int> par_;
 
-    UnionFind(int sz) : size_(sz), par(sz, -1) {}
+  public:
+    UnionFind() = default;
+    explicit UnionFind(int sz) : size_(sz), par_(sz, -1) {}
 
     // return root of x
     int root(int x) {
-        if (par[x] < 0) return x;
-        return par[x] = root(par[x]);
+        if (par_[x] < 0) return x;
+        return par_[x] = root(par_[x]);
     }
 
     // merge x and y
     bool merge(int x, int y) {
         x = root(x), y = root(y);
         if (x == y) return false;
-        if (par[x] > par[y]) std::swap(x, y);
-        par[x] += par[y];
-        par[y] = x;
+        if (par_[x] > par_[y]) std::swap(x, y);
+        par_[x] += par_[y];
+        par_[y] = x;
         return true;
     }
 
@@ -29,7 +32,7 @@ struct UnionFind {
     bool same(int x, int y) { return root(x) == root(y); }
 
     // return size of group including x
-    int size(int x) { return -par[root(x)]; }
+    int size(int x) { return -par_[root(x)]; }
 
     // return groups
     std::vector<std::vector<int>> groups() {
