10d70c131d2ab1c13644574928d7f64d683fe34f
kawaii-kawai <kawaiatsuki2008@gmail.com>
2023-10-09 21:35:23 +0900

    [change]Compressの仕様変更

--- other/compress.hpp
+++ other/compress.hpp
@@ -3,35 +3,33 @@
 #include <algorithm>
 #include <vector>
 
-template <class T> std::vector<T> compress(std::vector<T>& vec) {
-    std::vector<T> vals = vec;
-    std::sort(vals.begin(), vals.end());
-    vals.erase(std::unique(vals.begin(), vals.end()), vals.end());
-    for (auto& e : vec) {
-        e = std::lower_bound(vals.begin(), vals.end(), e) - vals.begin();
-    }
-    return vals;
-}
+template <class T> class Compress {
+  private:
+    std::vector<T> raws_;
 
-template <class T>
-std::vector<T> compress2d(std::vector<T>& c1, std::vector<T>& c2, int D = 1) {
-    std::vector<T> vals;
-    const int n = (int)(c1.size());
-    for (int i = 0; i < n; i++) {
-        for (int d = 0; d <= D; d++) {
-            T tc1 = c1[i] + d;
-            T tc2 = c2[i] + d;
-            vals.emplace_back(tc1);
-            vals.emplace_back(tc2);
-        }
+  public:
+    Compress(const std::vector<T>& raws = {}) : raws_(raws) {}
+
+    void add(const T& x) { raws_.emplace_back(x); }
+
+    void build() {
+        std::sort(raws_.begin(), raws_.end());
+        raws_.erase(std::unique(raws_.begin(), raws_.end()), raws_.end());
     }
-    std::sort(vals.begin(), vals.end());
-    vals.erase(std::unique(vals.begin(), vals.end()), vals.end());
-    for (int i = 0; i < n; i++) {
-        c1[i] =
-            std::lower_bound(vals.begin(), vals.end(), c1[i]) - vals.begin();
-        c2[i] =
-            std::lower_bound(vals.begin(), vals.end(), c2[i]) - vals.begin();
+
+    T operator[](int k) const { return raws_[k]; }
+
+    int get(const T& x) const {
+        return std::lower_bound(raws_.begin(), raws_.end(), x) - raws_.begin();
     }
-    return vals;
-}
+
+    int lower_bound(const T& x) const {
+        return std::lower_bound(raws_.begin(), raws_.end(), x) - raws_.begin();
+    }
+
+    int upper_bound(const T& x) const {
+        return std::upper_bound(raws_.begin(), raws_.end(), x) - raws_.begin();
+    }
+
+    int size() const { return (int)(raws_.size()); }
+};
--- test/aoj/dsl/DSL_4_A.test.cpp
+++ test/aoj/dsl/DSL_4_A.test.cpp
@@ -10,19 +10,32 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> x1[i] >> y1[i] >> x2[i] >> y2[i];
     }
-    auto xs = compress2d(x1, x2, 0);
-    auto ys = compress2d(y1, y2, 0);
-    CumulativeSum2D<long long> cs((int)(xs.size()), (int)(ys.size()));
+    Compress<long long> xs, ys;
+    for (int i = 0; i < n; i++) {
+        xs.add(x1[i]);
+        xs.add(x2[i]);
+        ys.add(y1[i]);
+        ys.add(y2[i]);
+    }   
+    xs.build();
+    ys.build();
+    CumulativeSum2D<long long> cs(xs.size(), ys.size());
+    for (int i = 0; i < n; i++) {
+        x1[i] = xs.get(x1[i]);
+        x2[i] = xs.get(x2[i]);
+        y1[i] = ys.get(y1[i]);
+        y2[i] = ys.get(y2[i]);
+    }
     for (int i = 0; i < n; i++) {
         cs.imos(x1[i], y1[i], x2[i], y2[i], 1);
     }
-
     cs.build();
     long long ans = 0;
-    for (int i = 0; i < (int)(xs.size()) - 1; i++) {
-        for (int j = 0; j < (int)(ys.size()) - 1; j++) {
-            if (cs.imos_get(i, j))
+    for (int i = 0; i < xs.size() - 1; i++) {
+        for (int j = 0; j < ys.size() - 1; j++) {
+            if (cs.imos_get(i, j)) {
                 ans += (xs[i + 1] - xs[i]) * (ys[j + 1] - ys[j]);
+            }
         }
     }
     cout << ans << endl;
